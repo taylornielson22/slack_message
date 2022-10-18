373b17a8a1db5e3813437ad56cfb60ab8696b867
Taylor Nielson <nielson.taylorm@gmail.com>
2022-10-18 15:54:36 -0600

    add slack_message.js

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { exec } = require("child_process");
-var Payload = require('./payload').Payload
+var request = require('request');
+const SlackMessage = require('./slack_message').SlackMessage;
+try {
+     const payload = github.context.payload;
+    // const review = payload.review;
+     const pr = payload.pull_request;
 
-const slack_payload = new Payload(core.getInput("channel"), core.getInput("username"))
-var message_input = core.getInput("message")
-const payload = github.context.payload;
-
-if (message_input.length > 0) {
-    core.info("Using message input variable to use as slack message")
-    slack_payload.set_message(message_input)
-} else {
-    pr_title = `PR <${payload.pull_request.html_url}|*#${payload.pull_request.number}* ${pull_request.title}>`
-    if (payload.review) {
-        var reviewer = `<${payload.review.user.url}|${payload.review.user.login}>`
-        slack_payload.set_message_attachment(review_message(payload.review.state, reviewer, pr_title), payload.review.body)
-    } else if (payload.pull_request.state = "open")
-        slack_payload.set_message_attachment(":rocket: New PR ready for review! :rocket:", `${pr_title} submitted by <${payload.pull_request.user.url}|${payload.pull_request.user.login}>`)
+    var slack_message = new SlackMessage(core.getInput("username"), core.getInput("slack_webhook"), core.getInput("channel"))
+    if(message.length > 0)
+        slack_message.custom_message(core.getInput("message_title"), core.getInput("message"))
+    else if (event_name.indexOf("pull_request") >= 0 && pr) {
+        pr_title = `<${pr.html_url}|*#${pr.number}* ${pr.title}>`
+        if(github.event_name == "pull_request" && pr.state=="open")
+            slack_message.pr_ready_message(pr_title, `<${pr.user.url}|${pr.user.login}>`)
+        else if(event_name == "pull_request_review")
+            slack_message.pr_review_message(payload.review.state, pr_title, `<${payload.review.user.url}|${payload.review.user.login}>`)
+        else if(event_name == "pull_request_review_comment")
+                slack_message.pr_review_commemt_message(pr_title, `<${payload.comment.user.url}|${payload.comment.user.login}>`, payload.comment.body)
+    }
+    request(slack_message.payload(), function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body) 
+        }
+      });
+} catch (error) {
+    console.log(error)
+    //core.setFailed(error.message);
 }
-json_payload = slack_payload.create_payload_file()
-exec(`curl -X POST ${core.getInput("message")} -H "Content-Type: application/json" -d ${json_payload}`, (error, stdout, stderr) => {
-    if (error)
-        console.error(error);
-    else if (stderr)
-        console.error(`stderr: ${stderr}`);
-    else
-        console.log(`stdout:\n${stdout}`);
-});
-
---
+++ slack_message.js
@@ -0,0 +1,82 @@
+
+const core = require("@actions/core");
+
+const json_payload = {
+        uri: "",
+        headers: {'content-type' : 'application/json'},
+        method: 'POST',
+        json: {
+            "channel": "",
+            "username": "",
+            "text": "",
+        }
+}
+const attachment = [
+           {
+              "fallback":"",
+              "pretext":"",
+              "color":"#D00000",
+              "fields":[
+                 {
+                    "value":"",
+                    "short":false
+                 }
+              ]
+           }
+        ]
+
+class SlackMessage
+{
+    constructor(username, webhook, channel)
+    {
+        this.payload = json_payload
+        this.payload.json.username = username
+        this.payload.uri = webhook
+        this.payload.json.channel = channel
+    }
+
+    pr_ready_message(pr_title, user) 
+    {
+        attachment[0].pretext = ":rocket: New PR ready for review! :rocket:"
+        attachment[0].fallback = ":rocket: New PR ready for review! :rocket:"
+        attachment[0].fields[0].value = `${pr_title} submitted by ${user} is ready for review`
+        this.payload.json["attachments"]= attachment
+        
+	}
+
+    pr_review_message(state, pr_title, review_user) 
+    {
+        if (state == "approved") 
+            this.payload.json.text = `PR ${pr_title} was approved by ${review_user}! :heavy_check_mark:`
+        else if (state == "changes_requested"){
+            attachment[0].pretext = `PR ${pr_title} was rejected!`
+            attachment[0].fallback = `PR ${pr_title} was rejected!`
+            attachment[0].fields[0].value =  `Changes were requested by ${user} on PR ${pr_title}`
+            this.payload.json["attachments"]= attachment
+        }
+	}
+    pr_review_commemt_message(pr_title, comment_user, body){
+        attachment[0].pretext = `${comment_user} has made a comment on PR ${pr_title}`
+        attachment[0].fallback = `${comment_user} has made a comment on PR ${pr_title}`
+        attachment[0].fields[0].value =  body
+        this.payload.json["attachments"]= attachment
+
+    }
+    custom_message(title, message){
+        if(title.length == 0)
+            this.payload.json.text = message
+        else{
+            attachment[0].pretext = title
+            attachment[0].fallback = title
+            attachment[0].fields[0].value = message
+            this.payload.json["attachments"]= attachment
+        }
+    }
+}
+	
+
+
+module.exports = {
+    SlackMessage
+}
+
