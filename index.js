1cefaaa1da8845ac495531498616565caa5e1d72
taylornielson22 <53064096+taylornielson22@users.noreply.github.com>
2022-10-20 13:28:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const SlackMessage = require('./slack_message').SlackMessage;
 try {
     const payload = github.context.payload;
     const pr = payload.pull_request;
-    const review = payload.review;
     console.log(`Event Trigger : ${github.context.event_name}`)
     var slack_message = new SlackMessage(core.getInput("username"), core.getInput("slack_webhook"), `#${core.getInput("channel")}`)
     if(core.getInput("message").length > 0)
         slack_message.custom_message(core.getInput("message_title"), core.getInput("message"))
     else if (pr) {
         pr_title = `<${pr.html_url}|*#${pr.number}* ${pr.title}>`
-        if(review)
-            slack_message.pr_review_message(payload.review.state, pr_title, `<${review.user.url}|${review.user.login}>`)
+        if(payload.review)
+            slack_message.pr_review_message(payload.review.state, pr_title, `<${payload.review.user.url}|${payload.review.user.login}>`, payload.review.body)
         else if(pr.state=="open")
             slack_message.pr_ready_message(pr_title, `<${pr.user.url}|${pr.user.login}>`)
     }
