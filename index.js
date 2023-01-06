2a9494b8bb663a739bf659d23b485421c1ebf071
taylornielson22 <53064096+taylornielson22@users.noreply.github.com>
2023-01-05 17:16:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-var request = require('request');
 const SlackMessage = require('./slack_message').SlackMessage;
+
 try {
     const payload = github.context.payload;
     const pr = payload.pull_request;
-    console.log(`Event Trigger : ${github.context.event_name}`)
     var slack_message = new SlackMessage(core.getInput("username"), core.getInput("slack_webhook"), `#${core.getInput("channel")}`)
     if(core.getInput("message").length > 0)
         slack_message.create_message(core.getInput("message_title"), core.getInput("message"))
@@ -16,15 +15,8 @@ try {
         else if(pr.state=="open")
             slack_message.pr_message("", pr_title, `<${pr.user.url}|${pr.user.login}>`, "")
     }
-     
-    console.log(`Sending payload to chanel #${core.getInput("channel")}`)
-    console.log(`Payload:\n${slack_message.payload}`)
-    request(slack_message.payload, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          console.log(body) 
-        }
-     });
+    console.log(`Sending message to channel #${core.getInput("channel")}`)
+    slack_message.send_message()
 } catch (error) {
     console.log(error)
-    //core.setFailed(error.message);
 }
