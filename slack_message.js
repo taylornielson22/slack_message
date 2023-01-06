b37582b357c65dd9ca0792da78190eea76044344
taylornielson22 <53064096+taylornielson22@users.noreply.github.com>
2023-01-05 17:15:48 -0700

    Update slack_message.js

--- slack_message.js
+++ slack_message.js
@@ -1,6 +1,6 @@
 
 const core = require("@actions/core");
-
+const { exec } = require("child_process");
 const json_payload = {
     uri: "",
     headers: {'content-type' : 'application/json'},
@@ -18,6 +18,7 @@ const attachment = [{
         value:"",
         short:false 
     }]}]
+
 
 class SlackMessage
 {
@@ -39,26 +40,36 @@ class SlackMessage
         }
     }
 
-    pr_message(state, pr_title, user, body) 
-    {
-	switch(state){
-	    case "approved":
+    pr_message(state, pr_title, user, body) {
+	    switch(state){
+	        case "approved":
                 this.create_message("",`PR ${pr_title} was approved by ${user}! :cool-dog:`)
-		break;
+		        break;
             case "changes_requested":
             	this.create_message(`${user} requested changes on PR ${pr_title}`, body)
-		break;
-	    case "commented":
-	        this.create_message(`${user} left a comment on PR ${pr_title}`, body)
-	        break;
-	    default:
-	        this.create_message(":rocket: New PR ready for review! :rocket:", `${pr_title} submitted by ${user} is ready for review`)
-		attachment[0].color = "#D00000"	
-	}
+		        break;
+	        case "commented":
+	            this.create_message(`${user} left a comment on PR ${pr_title}`, body)
+	            break;
+	        default:
+	            this.create_message(":rocket: New PR ready for review! :rocket:", `${pr_title} submitted by ${user} is ready for review`)
+		        attachment[0].color = "#D00000"	
+	    }
+   }
+
+   send_message() {
+	    var json_payload = JSON.stringify(this.payload.json)
+	    exec(`curl -X POST ${this.payload.uri} -H "Content-Type: application/json" --data '${json_payload}'`, (error, stdout, stderr) => {
+    	    	if (error)
+        	    core.setFailed(error);
+    	    	else if (stderr)
+        	    core.setFailed(`stderr: ${stderr}`);
+    	    	else
+        	    console.log(`stdout:\n${stdout}`);
+	    });
    }
 }
 
 module.exports = {
     SlackMessage
 }
-
